57a00732d4fb6adf41e428f5ec9a503a199e4bcd
unknown <abdoalrhman9@hotmail.com>
2022-02-22 03:43:04 -0800

    fix image resources

--- resources/views/patient/home.blade.php
+++ resources/views/patient/home.blade.php
@@ -1,7 +1,7 @@
 @extends('layouts.app')
 
 @section('content')
-    <section class="navigator" ></section>
+    <section class="navigator" style="background-image: url({{ asset('hero.jpg') }});"></section>
     <div class="container-fluid">
         <div class="row justify-content-center align-items-start">
             @include('layouts.patient-sidebar')
