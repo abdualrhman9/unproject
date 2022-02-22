2310bb6851404cf8efb49172e63c9039c6fb7b6b
unknown <abdoalrhman9@hotmail.com>
2022-02-22 05:41:25 -0800

    master fix

--- routes/api.php
+++ routes/api.php
@@ -20,7 +20,7 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::middleware(['auth:sanctum'])->prefix('patients')->name('patients.')->group(function(){
+Route::prefix('patients')->name('patients.')->group(function(){
     Route::post('diseases/select',[PatientDiseaseController::class,'select'])->name('diseases.select');
     Route::post('doctors/select',[PatientDoctorController::class,'select'])->name('doctors.select');
 });
